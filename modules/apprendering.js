a149efd000b88c9f2d7750115962fb89319c80a8
Alberto <halbtale@gmail.com>
2021-08-13 12:30:39 +0200

    Added

--- modules/apprendering.js
+++ modules/apprendering.js
@@ -1,8 +1,10 @@
 const appContent = document.getElementById('app-content');
 
 export const displayElements = (elements) => {
-    appContent.innerHTML = '';
-    elements.forEach((element) => {
-        appContent.insertAdjacentHTML('beforeend', `<app-card title="${element.title}" description="${element.description}" imgUrl="${element.imgUrl}"></app-card>`);
-    })
+    if (elements.length) {
+        appContent.innerHTML = '';
+        elements.forEach((element) => {
+            appContent.insertAdjacentHTML('beforeend', `<app-card title="${element.title}" description="${element.description}" imgUrl="${element.imgUrl}"></app-card>`);
+        })
+    }
 }
