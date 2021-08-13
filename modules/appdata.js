a82736211c9f4f3fe66a0c5eb9f7cf595e80904f
Alberto <halbtale@gmail.com>
2021-08-13 12:28:11 +0200

    ADded

--- modules/appdata.js
+++ modules/appdata.js
@@ -11,3 +11,15 @@ appData.set('#11', {
     description: 'Fa male',
     imgUrl: 'assets/sigaretta.jpg'
 });
+
+appData.set('#12', {
+    title: 'Carte',
+    description: 'Da gioco',
+    imgUrl: 'assets/carte.jpg'
+});
+
+appData.set('#13', {
+    title: 'Sedie Ribaltate',
+    description: 'Good luck',
+    imgUrl: 'assets/sedia.jpg'
+});
